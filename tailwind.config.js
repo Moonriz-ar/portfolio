a6a622c12a2de5a4e0d5f53f192661ea24735793
Andrea Lin <andre93.lin@gmail.com>
2022-04-23 11:34:24 -0300

    set up file structure and font

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ibm: ["IBM Plex Serif", "serif"],
+        square: ["Square Peg", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
