ffc7fe72bf7a0b874237642056e396469321c38a
Andrea Lin <andre93.lin@gmail.com>
2022-04-27 08:43:21 -0300

    Contact.js
    Centered the contact card

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -11,11 +11,11 @@ function Contact() {
       className="bg-mobile-contact-background sm:bg-desktop-contact-background bg-cover min-h-screen"
     >
       <div className="min-h-screen bg-gradient-to-b from-white flex flex-col justify-start items-center px-5">
-        <div className="w-full sm:max-w-3xl px-5 sm:px-16 pt-10 pb-14 rounded-b-3xl flex flex-col bg-slate-50">
+        <div className="w-full sm:max-w-3xl px-5 sm:px-16 pt-10 pb-14 rounded-b-3xl flex flex-col items-center bg-slate-50">
           <Title title="Contact." />
 
           {/* Contact me card */}
-          <section className="text-slate-200 space-y-5 sm:max-w-sm flex flex-col">
+          <section className="text-slate-200 space-y-5 sm:w-1/2 flex flex-col">
             <article className="bg-gradient-to-r from-slate-900  to-slate-600 rounded-xl p-5 drop-shadow">
               <div>
                 <h4 className="font-square text-5xl am:text-4xl font-bold pb-1">
